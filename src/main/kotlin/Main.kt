aaf2a2abc9b61165eaae54eac17480743727d8f6
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-02-10 11:22:50 +0300

    created a kotlin file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    // This is practice
+    println("Hello world") // This is the first code//
+    println("I am learning kotlin")
+    println("3*3")
+    var name =  "Agnes"
+    println ("Name : Agnes")
+    var yearOfBirth= ("2002")
+    println ("Year Of Birth : 2002")
+    var nameRun : String = "Agnes Auma Kamonndi "
+    println ("Name Run : Agnes Auma Kamondi")
+    var yearOfKuzaliwa: Int = 2002
+    println("Year of Kuzaliwa : 2002")
+    var classes = arrayOf("Lovelace","Anitab","ada")
+    println(" Classes: Lovelace, anitab,ada")
+    var bag : String = "lovelace, Anitab and ada are the classes we have in AkiraChix"
+    var firstName ="Agnes"
+    var secondName = "Auma"
+    var thirdName = "Kamondi"
+    var fullName = firstName + secondName + thirdName
+    println(fullName)
+    val kenyanCitizen : Boolean =true
+    println(kenyanCitizen)
+    val x = 10
+    val y = 20
+    println(x > y)
+
+}
